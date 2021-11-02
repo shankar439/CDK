9d46f9f2a7c7d3741d8865581e20980cb3945977
SHANKAR <shankarkj439@gmail.com>
2021-11-02 23:25:16 +0530

    Initial commit

---
+++ src/main/java/com/Coherent/CDK/CdkApplication.java
@@ -0,0 +1,13 @@
+package com.Coherent.CDK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Coherent/CDK/CdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Coherent.CDK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
