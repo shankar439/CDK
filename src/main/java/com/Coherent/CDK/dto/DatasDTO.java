4beb010aed632372218d9ed5fab63519b79c2095
vignesh6645 <gvignesh643@gmail.com>
2021-11-05 13:14:24 +0530

    vigneshG commit

---
+++ src/main/java/com/Coherent/CDK/dto/DatasDTO.java
@@ -0,0 +1,13 @@
+package com.Coherent.CDK.dto;
+
+import lombok.Data;
+
+@Data
+public class DatasDTO {
+
+    private String name;
+
+    private String data;
+
+    private Short filesId;
+}
--- src/main/java/com/Coherent/CDK/response/BaseResponse.java
+++ src/main/java/com/Coherent/CDK/response/BaseResponse.java
@@ -1,16 +1,18 @@
 package com.Coherent.CDK.response;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class BaseResponse<T> {
+
     private int statusCode = 200;
+
     private String statusMessage = "SUCCESS";
+
     private T data;
 }
